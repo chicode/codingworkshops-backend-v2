617d59ecb171d34f0c45aef11c8b5ff829536d38
Anton <antonoutkine@gmail.com>
2019-05-19 16:02:59 -0500

    Fix seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,31 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Workshops.Repo
+alias Workshops.User
+alias Workshops.Workshop
+
+if workshop = Repo.get_by(Workshop, name: "Nico") do
+  Repo.delete!(workshop)
+end
+
+if user = Repo.get_by(User, username: "username") do
+  Repo.delete!(user)
+end
+
+User.create_changeset(%User{}, %{
+  username: "username",
+  password: "123456",
+  email: "test@test.com"
+})
+|> Repo.insert!()
+|> Ecto.build_assoc(:workshops)
+|> Workshop.create_changeset(%{
+  name: "Nico",
+  description: "cool workshop",
+  source_url: "https://raw.githubusercontent.com/chicode/workshops/master/nico.yaml"
+})
+|> Repo.insert!()
+|> WorkshopsWeb.WorkshopController.load_workshop()
+|> IO.inspect()
